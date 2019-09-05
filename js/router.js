cd471f94984d616c519b232722aca42b6312b118
Will <willtaylor.developer@gmail.com>
2019-09-05 10:23:03 +0100

    Use slice instead of splice and other minor refactorings.

--- js/router.js
+++ js/router.js
@@ -9,9 +9,10 @@ class Router {
     // Get the template for the given route.
     const matchedRoute = this._matchUrlToRoute(urlSegments);
 
-    // Push a history entry, with the url of the matched route. 
+    // Push a history entry with the new url.
     // We pass an empty object and an empty string as the historyState and title arguments, but their values do not really matter here.
-    history.pushState({}, '', matchedRoute.path);
+    const url = `/${urlSegments.join('/')}`;
+    history.pushState({}, '', url);
 
     // Append the given template to the DOM inside the router outlet.
     const routerOutletElement = document.querySelectorAll('[data-router-outlet]')[0];
@@ -25,7 +26,7 @@ class Router {
     const matchedRoute = this.routes.find(route => {
       // Assume that the route path always starts with a slash, and so the first item in the segments array 
       // will always be an empty string. Splice the array to ignore this empty string.
-      const routePathSegments = route.path.split('/').splice(1);
+      const routePathSegments = route.path.split('/').slice(1);
       // If there are different numbers of segments, then the route does not match the URL.
       if (routePathSegments.length !== urlSegments.length) {
         return false;
@@ -40,7 +41,7 @@ class Router {
   _loadInitialRoute() {
     // Figure out the path segments for the route which should load initially.
     const pathnameSplit = window.location.pathname.split('/');
-    const pathSegments = pathnameSplit.length > 1 ? pathnameSplit.splice(1) : '';
+    const pathSegments = pathnameSplit.length > 1 ? pathnameSplit.slice(1) : '';
     // Load the initial route.
     this.loadRoute(...pathSegments );
   }
